8907d03c7f596d5dd70f5a0e84fa8d959df18267
Benoit VALLON <benoitvallon@gmail.com>
2015-05-14 17:53:05 -0400

    Adds autocomplete to fields

--- public/modules/pressbooks/controllers/pressbooks.client.controller.js
+++ public/modules/pressbooks/controllers/pressbooks.client.controller.js
@@ -4,6 +4,11 @@ angular.module('pressbooks').controller('PressbooksController', ['$scope', '$sta
   function($scope, $stateParams, $location, Pressbooks, $http) {
     $scope.pressbookTempo = {};
 
+    $scope.placeholder1 = {};
+    $scope.placeholder2 = {};
+    $scope.placeholder3 = {};
+    $scope.placeholder4 = {};
+
     $scope.remove = function(pressbook) {
       if (pressbook) {
         pressbook.$remove();
@@ -21,22 +26,23 @@ angular.module('pressbooks').controller('PressbooksController', ['$scope', '$sta
     };
 
     $scope.update = function(pressbook) {
-      if($scope.pressbookTempo.placeholder1) {
-        pressbook.placeholder1 = $scope.pressbookTempo.placeholder1;
-        $scope.pressbookTempo.placeholder1 = '';
+      if($scope.placeholder1.value) {
+        pressbook.placeholder1 = $scope.placeholder1.value;
+        $scope.placeholder1.value = '';
       }
-      if($scope.pressbookTempo.placeholder2) {
-        pressbook.placeholder2 = $scope.pressbookTempo.placeholder2;
-        $scope.pressbookTempo.placeholder2 = '';
+      if($scope.placeholder2.value) {
+        pressbook.placeholder2 = $scope.placeholder2.value;
+        $scope.placeholder2.value = '';
       }
-      if($scope.pressbookTempo.placeholder3) {
-        pressbook.placeholder3 = $scope.pressbookTempo.placeholder3;
-        $scope.pressbookTempo.placeholder3 = '';
+      if($scope.placeholder3.value) {
+        pressbook.placeholder3 = $scope.placeholder3.value;
+        $scope.placeholder3.value = '';
       }
-      if($scope.pressbookTempo.placeholder4) {
-        pressbook.placeholder4 = $scope.pressbookTempo.placeholder4;
-        $scope.pressbookTempo.placeholder4 = '';
+      if($scope.placeholder4.value) {
+        pressbook.placeholder4 = $scope.placeholder4.value;
+        $scope.placeholder4.value = '';
       }
+      updateAcPlaceholderValues(pressbook);
 
       pressbook.$update(function(err) {
         pressbook.edit = false;
@@ -46,7 +52,26 @@ angular.module('pressbooks').controller('PressbooksController', ['$scope', '$sta
     };
 
     $scope.find = function() {
-      $scope.pressbooks = Pressbooks.query();
+      $scope.pressbooks = Pressbooks.query(function(data) {
+        $scope.pressbooks.forEach(function(pressbook) {
+          updateAcPlaceholderValues(pressbook);
+        });
+      });
+    };
+
+    var updateAcPlaceholderValues = function(pressbook) {
+      if(pressbook.placeholder1 &&
+          !~$scope.acPlaceholder1Values.indexOf(pressbook.placeholder1)) {
+        $scope.acPlaceholder1Values.push(pressbook.placeholder1);
+      }
+      if(pressbook.placeholder2 &&
+          !~$scope.acPlaceholder2Values.indexOf(pressbook.placeholder2)) {
+        $scope.acPlaceholder2Values.push(pressbook.placeholder2);
+      }
+      if(pressbook.placeholder3 &&
+          !~$scope.acPlaceholder3Values.indexOf(pressbook.placeholder3)) {
+        $scope.acPlaceholder3Values.push(pressbook.placeholder3);
+      }
     };
 
     $scope.findOne = function() {
@@ -75,20 +100,72 @@ angular.module('pressbooks').controller('PressbooksController', ['$scope', '$sta
       pressbook.edit = !pressbook.edit;
 
       if(pressbook.edit) {
-        $scope.pressbookTempo.placeholder1 = pressbook.placeholder1;
-        $scope.pressbookTempo.placeholder2 = pressbook.placeholder2;
-        $scope.pressbookTempo.placeholder3 = pressbook.placeholder3;
-        $scope.pressbookTempo.placeholder4 = pressbook.placeholder4;
+        $scope.placeholder1.value = pressbook.placeholder1;
+        $scope.placeholder2.value = pressbook.placeholder2;
+        $scope.placeholder3.value = pressbook.placeholder3;
+        $scope.placeholder4.value = pressbook.placeholder4;
       } else {
-        $scope.pressbookTempo.placeholder1 = '';
-        $scope.pressbookTempo.placeholder2 = '';
-        $scope.pressbookTempo.placeholder3 = '';
-        $scope.pressbookTempo.placeholder4 = '';
+        $scope.placeholder1.value = '';
+        $scope.placeholder2.value = '';
+        $scope.placeholder3.value = '';
+        $scope.placeholder4.value = '';
       }
     };
 
     $scope.updateText = function() {
       $scope.textTemplateEncoded = encodeURIComponent($scope.textTemplate);
+    };
+
+    $scope.acPlaceholder1Values = [];
+    $scope.acPlaceholder2Values = [];
+    $scope.acPlaceholder3Values = [];
+
+    function suggestPlaceholder1(term) {
+      var q = term.toLowerCase().trim();
+      var results = [];
+      // Find first 10 acPlaceholder1 that start with `term`.
+      for (var i = 0; i < $scope.acPlaceholder1Values.length && results.length < 10; i++) {
+        var state = $scope.acPlaceholder1Values[i];
+        if (state.toLowerCase().indexOf(q) === 0)
+          results.push({ label: state, value: state });
+      }
+      return results;
+    }
+
+    $scope.acPlaceholder1 = {
+      suggest: suggestPlaceholder1
+    };
+
+    function suggestPlaceholder2(term) {
+      var q = term.toLowerCase().trim();
+      var results = [];
+      // Find first 10 acPlaceholder2 that start with `term`.
+      for (var i = 0; i < $scope.acPlaceholder2Values.length && results.length < 10; i++) {
+        var state = $scope.acPlaceholder2Values[i];
+        if (state.toLowerCase().indexOf(q) === 0)
+          results.push({ label: state, value: state });
+      }
+      return results;
+    }
+
+    $scope.acPlaceholder2 = {
+      suggest: suggestPlaceholder2
+    };
+
+    function suggestPlaceholder3(term) {
+      var q = term.toLowerCase().trim();
+      var results = [];
+      // Find first 10 acPlaceholder3 that start with `term`.
+      for (var i = 0; i < $scope.acPlaceholder3Values.length && results.length < 10; i++) {
+        var state = $scope.acPlaceholder3Values[i];
+        if (state.toLowerCase().indexOf(q) === 0)
+          results.push({ label: state, value: state });
+      }
+      return results;
+    }
+
+    $scope.acPlaceholder3 = {
+      suggest: suggestPlaceholder3
     };
   }
 ]);
